9cd5d76f4247b38490bd9cd4eab81a06e98eeb30
Evgeny <94202448+Acidastro@users.noreply.github.com>
2023-11-06 15:44:40 +0400

    Create example_in_class.py

---
+++ example_in_class.py
@@ -0,0 +1,19 @@
+@pytest.fixture
+def fixture_mock_get_by_id_client_2():
+    mock_get_by_id = Mock()
+    mock_get_by_id.return_value = get_file(path_ + 'client_categories_1.json')
+    with patch.object(Clients, 'get_by_id', new=mock_get_by_id):
+        yield
+
+@pytest.mark.asyncio
+async def test_check_client_category_1(fixture_mock_get_by_id_client_2):
+    example = Example()
+    res = await checker.check_client_category()
+    assert res
+
+
+class Example:
+  
+  async def check_client_category(self):
+    client = target.Clients(self.account.crm_data).get_by_id(self.record.data.client.id)
+    return True
