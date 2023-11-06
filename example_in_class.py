b3e6fe4ca618c39ac1a5463f15aff696dc4308d8
Evgeny <94202448+Acidastro@users.noreply.github.com>
2023-11-06 15:56:08 +0400

    Update example_in_class.py

--- example_in_class.py
+++ example_in_class.py
@@ -1,12 +1,13 @@
 @pytest.fixture
-def fixture_mock_get_by_id_client_2():
+def fixture_mock_get_by_id_client_1():
     mock_get_by_id = Mock()
     mock_get_by_id.return_value = get_file(path_ + 'client_categories_1.json')
-    with patch.object(Clients, 'get_by_id', new=mock_get_by_id):
+    with patch.object(Clients, 'get_by_id', new=mock_get_by_id), \
+            patch.object(CheckMsg, 'conn', new_callable=PropertyMock(return_value=None)):
         yield
 
 @pytest.mark.asyncio
-async def test_check_client_category_1(fixture_mock_get_by_id_client_2):
+async def test_check_client_category_1(fixture_mock_get_by_id_client_1):
     example = Example()
     res = await checker.check_client_category()
     assert res
